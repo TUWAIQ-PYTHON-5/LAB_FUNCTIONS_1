4863a3e041d3c3965dccf9c35df5a3f62154210d
Mohammed <al5aldi.jav@gmail.com>
2023-01-11 12:31:28 +0300

    Added second Ex

--- func.py
+++ func.py
@@ -1,10 +1,19 @@
-
+#Create a function 
 def myFunc(num):
-    
+
     for i in range(num,0,-1):
         for j in range(i , 0 , -1 ): 
             print(j , end= " ")
         print("")
-             
+   
 myFunc(5)
+
+print("-----------------------------")
+
+#describe what it does
+def somePrint()-> None:
+    """Print something"""
+    print("hello world")
+
+somePrint()
 
