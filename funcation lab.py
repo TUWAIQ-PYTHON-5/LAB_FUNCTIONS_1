8451d7c63a63306bdf71ded47676da8a0dd1b455
sultan <sultan_33_886@outlook.sa>
2023-01-11 14:01:32 +0300

    lab functions

---
+++ funcation lab.py
@@ -0,0 +1,16 @@
+def varbiles_number(x:int):
+    ''' the number of funcations'''
+    for a in range(x,0,-1) :
+        for b in range(a,0,-1):
+            print(b ,end=" ")
+        print(" ")   
+varbiles_number(5)
+print(varbiles_number.__doc__)
+
+
+
+    
+             
+     
+
+        
