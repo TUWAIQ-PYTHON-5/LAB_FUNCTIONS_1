b7f6d1c0be7f06f5383867dc4e39b847c847bdf6
Faimubarak <102253601+Faimubarak@users.noreply.github.com>
2023-01-11 15:34:07 +0300

    Add files via upload

--- lab function.py
+++ lab function.py
@@ -1,12 +1,11 @@
 
 def num1(n : int):
   '''  '''
-  num = n
-  return num
-num = 5
-
-for i in range(0, num):
-    for j in range(num - i,0, -1):
+ 
+  for i in range(n, 0,-1):
+    for j in range(i, 0, -1):
         print(j, end="")
     print()
+
+num1(5)
 print(num1.__doc__)
