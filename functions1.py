bfe2d324eecd6b2f72a104a17ee0c99b15ae88f9
NjoudAlhazmi <njoudz19@gmail.com>
2023-01-11 13:04:24 +0300

    sol

---
+++ functions1.py
@@ -0,0 +1,11 @@
+
+def function(y : int ) -> int:
+    ''' here where i can put  the doc '''
+    par = y
+    return par
+x = 5
+for i  in range(0, 5):
+    for j in range(x - i, 0, -1):
+        print(j, end=' ')
+    print()
+print(function.__doc__)
