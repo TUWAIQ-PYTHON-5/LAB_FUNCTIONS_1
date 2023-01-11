979dabe4a36d85d35a5557c7c82b227c606f55dd
MOH7117 <mohammad201027@hotmail.com>
2023-01-11 13:44:05 +0300

    func

--- func.py
+++ func.py
@@ -1,8 +1,10 @@
 
 def number(num):
-    for i in range(0 , num + 1):
+    '''print this function'''
+    for i in range(0,num+1):
         for x in range(num-i, 0 ,-1):
-            print(x , end=" ")
+            print(x, end=" ")
   
-        print("")
+        print(" ")
 number(5)
+print(number.__doc__)
